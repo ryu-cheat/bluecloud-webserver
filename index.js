7ed345112a77bb98285226e2431ed7830f23a11f
cheat <38871886+lingobong@users.noreply.github.com>
2024-10-08 10:28:34 +0900

    a

--- index.js
+++ index.js
@@ -5,6 +5,31 @@ const path = require('path');
 
 const app = express();
 
+const fs = require('fs');
+
+app.use((req, res, next) => {
+    const host = req.headers.host; // 요청된 호스트 이름
+
+    let subdomainsTxt = '';
+    try {
+        subdomainsTxt = fs.readFileSync(path.join(__dirname, 'subdomains.txt'), 'utf8');
+    } catch (e) { }
+
+    let lines = subdomainsTxt.split('\n');
+    for (let line of lines) {
+        let [_host, _redirect] = line.split(' -> ');
+        if (_host && _redirect) {
+            if (host === _host) {
+                return res.redirect(301, _redirect);
+            }
+        }
+    }
+
+    // 다른 요청은 기본 처리
+    next();
+});
+
+
 app.use(require('cors')());
 app.use(express.json());
 
